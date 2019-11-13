02f64e709298d1896b1c2530092f50da4ffc9d6b
Alex <alexandre.barbillon@gmail.com>
2019-11-13 10:31:18 +0100

    Adding annotations

--- src/main/java/com/ifi/tp/pokemon_type_api/controller/PokemonTypeController.java
+++ src/main/java/com/ifi/tp/pokemon_type_api/controller/PokemonTypeController.java
@@ -2,19 +2,22 @@ package com.ifi.tp.pokemon_type_api.controller;
 
 import com.ifi.tp.pokemon_type_api.bo.PokemonType;
 import com.ifi.tp.pokemon_type_api.service.PokemonTypeService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/pokemon-types")
 public class PokemonTypeController {
     private PokemonTypeService pokeService;
     public PokemonTypeController(PokemonTypeService service) {
         this.pokeService = service;
     }
 
-    public PokemonType getPokemonTypeFromId(int i) {
-        return pokeService.getPokemonType(i);
+    @GetMapping("/{id}")
+    public PokemonType getPokemonTypeFromId(@MatrixVariable int id) {
+        return pokeService.getPokemonType(id);
     }
-
+    @GetMapping("/")
     public List<PokemonType> getAllPokemonTypes() {
         return pokeService.getAllPokemonTypes();
     }
